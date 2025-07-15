9211fb9b093a62894f7a55cd117612edd84f6bd5
isushmeeta <sushmitah0199@gmail.com>
2025-07-15 16:07:37 +0600

    api addition: get, post, put, delete

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,22 @@ import express from 'express';
 const app = express();
 const PORT = 5001;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
+app.get('/api/careerconnect', (req, res) => {
+  res.status(200).send('Hello from Dhaka!');
+}); //api code 
 
+app.post("/api/notes",(req,res)=>{
+    res.status(201).json({message:"post created successfully"})
+})
+
+app.put("/api/notes:id",(req,res)=>{
+    res.status(200).json({message:"post updated successfully"})
+})
+
+app.delete("/api/notes:id",(req,res)=>{
+    res.status(200).json({message:"post deleted successfully"})
+})
+http://localhost:5001/api/notes/21
 app.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);
 });
