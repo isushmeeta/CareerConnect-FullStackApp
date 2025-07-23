02ed7e58b387275baea9c42406e41098b79a3cba
isushmeeta <sushmitah0199@gmail.com>
2025-07-23 14:57:17 +0600

    Job Routing backend first phase

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,12 @@
 import express from 'express';
+import jobroutes from "./routes/jobroutes.js"
 
 const app = express();
 const PORT = 5001;
 
-app.get('/api/careerconnect', (req, res) => {
-  res.status(200).send('Hello from Dhaka!');
-}); //api code 
+app.use("/api/jobs", jobroutes);
 
-app.post("/api/notes",(req,res)=>{
-    res.status(201).json({message:"post created successfully"})
-})
 
-app.put("/api/notes:id",(req,res)=>{
-    res.status(200).json({message:"post updated successfully"})
-})
-
-app.delete("/api/notes:id",(req,res)=>{
-    res.status(200).json({message:"post deleted successfully"})
-})
 http://localhost:5001/api/notes/21
 app.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);
